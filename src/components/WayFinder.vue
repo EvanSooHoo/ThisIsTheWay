159904ce436cf5c69734c374b7a230fd82ff429b
Evan SooHoo <evan.soohoo@gmail.com>
2022-02-05 10:05:10 -0800

    cut vuetify v-form example down to applicable search criteria

--- src/components/WayFinder.vue
+++ src/components/WayFinder.vue
@@ -8,41 +8,29 @@
     >
       <v-text-field
         v-model="name"
-        :counter="10"
-        :rules="nameRules"
         label="Name"
         required
       ></v-text-field>
 
       <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
+        v-model="homeworld"
+        label="Homeworld"
         required
       ></v-text-field>
 
-      <v-select
-        v-model="select"
-        :items="items"
-        :rules="[v => !!v || 'Item is required']"
-        label="Item"
+      <v-text-field
+        v-model="films"
+        label="Films"
         required
-      ></v-select>
-
-      <v-checkbox
-        v-model="checkbox"
-        :rules="[v => !!v || 'You must agree to continue!']"
-        label="Do you agree?"
-        required
-      ></v-checkbox>
+      ></v-text-field>
 
       <v-btn
         :disabled="!valid"
         color="success"
         class="mr-4"
-        @click="validate"
+        @click="search"
       >
-        Validate
+        Search
       </v-btn>
 
       <v-btn
@@ -52,16 +40,7 @@
       >
         Reset Form
       </v-btn>
-
-      <v-btn
-        color="warning"
-        @click="resetValidation"
-      >
-        Reset Validation
-      </v-btn>
     </v-form>
-    
-    
     </div>
   </template>
 
@@ -74,28 +53,13 @@
     data: () => ({
       valid: true,
       name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
+      homeworld: '',
+      films: ''
     }),
 
     methods: {
-      validate () {
-        this.$refs.form.validate()
+      search() {
+
       },
       reset () {
         this.$refs.form.reset()
